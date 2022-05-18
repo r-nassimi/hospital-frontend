8858bc59d2ba04d63117ad691840b1a0d02d0c9a
Rustam Nassimi <nassimi.exceedteam@gmail.com>
2022-05-18 09:39:21 +0300

    trying to push working project

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Registration from "src/components/Registration/Registration";
 import Login from "src/components/Login/Login";
-import "src/App/app.scss";
+import 'src/App/app.scss'
 
 const App = () => {
   return (
@@ -9,8 +9,9 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/registration" element={<Registration />} />
+        
         //Redirect from empty adress to login form
-        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/" element={<Navigate replace to = "/login"/>} />
       </Routes>
     </div>
   );
--- src/services/AuthorizationService.js
+++ src/services/AuthorizationService.js
@@ -1,16 +1,17 @@
 import api from "src/http/api";
 
 export default class AuthService {
+  
   //Static properties are properties of a class, not of an instance of a class.
-  static async registration(login, password) {
-    return api.post("/registration", { login, password });
+  static async registration (login, password) {
+    return api.post ("/registration", { login, password });
   }
 
-  static async login(login, password) {
-    return api.post("/login", { login, password });
+  static async login (login, password) {
+    return api.post ("/login", { login, password });
   }
 
-  static async logout() {
-    return api.post("/logout");
-  }
+  static async logout () {
+    return api.post ("/logout");
+  };
 }
